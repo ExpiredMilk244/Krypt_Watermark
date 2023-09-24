d1d13a5d58dcb95df53c7f11af98cb674a427783
Xpired Milk <37828403+ExpiredMilk244@users.noreply.github.com>
2023-09-24 22:05:31 +1000

    Initial commit

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+-- toggle
+Config.toggle = false -- allows players to enable/disable watermark
+
+-- logo
+Config.logo = false -- use logo (will override text)
+
+-- text options
+Config.text = "Change me" -- text
+Config.textRainbow = true -- rainbow colour (will override normal colour)
+Config.textColour = {r = 35, g = 153, b = 212} -- rgb colour values (https://www.rapidtables.com/web/color/RGB_Color.html)
+Config.textTransparency = 255 -- alpha values (0-255)
+Config.textFont = 1 -- font options range from 1-5
+Config.textScale = 0.4 -- size of text
+Config.textOffset = {x = 0.005, y = 0.001} -- alters position of text
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "cerulean"
+game "gta5"
+
+name "ud-watermark"
+description "Customisable Watermark"
+author "Umeni Developments"
+version "v1.0.0"
+
+client_scripts {
+	'client.lua'
+}
+
+shared_script 'config.lua'
+
+ui_page 'html/ui.html'
+
+files {
+	'html/*',
+	'html/img/*'
+}
